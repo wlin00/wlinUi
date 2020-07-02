5d9494ac924728b05f2a43e8298907abb0f644b1
wlin00 <616294069@qq.com>
2020-07-03 02:08:29 +0800

    完成buttonGroup组件，样式调整、以及mounted周期进行组件内部结果校验和警告

--- src/app.js
+++ src/app.js
@@ -2,16 +2,19 @@
 import Vue from 'vue'
 import Button from './button'
 import Icon from './icon'
+import ButtonGroup from './button-group'
 
 //声明全局的组件
 Vue.component('wlin-button',Button)
 Vue.component('wlin-icon',Icon)
+Vue.component('wlin-button-group',ButtonGroup)
 
 
 new Vue({
     el:'#app',
     data:{
         loading1:false,
-        loading2:false
+        loading2:false,
+        loading3:false
     }
 })
---
+++ src/button-group.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="wlin-button-group"><slot/></div>
+</template>
+
+<script>
+    export default {
+        mounted () {
+            for(let item of this.$el.children){
+                // if()
+                if(item.nodeName.toLowerCase() !== 'button'){
+                    console.warn(`buttonGroup组件内部需全部是button标签构成，当前您使用了错误的${item.nodeName.toLowerCase()}标签，请修改！`)
+                }
+            }
+        },
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+  .wlin-button-group{
+      display: inline-flex;
+      vertical-align: middle;
+      >.wlin-button{
+          border-radius: 0;
+          &:first-child{
+              border-top-left-radius: var(--border-radius);
+              border-bottom-left-radius: var(--border-radius);
+          }
+          &:last-child{
+              border-top-right-radius: var(--border-radius);
+              border-bottom-right-radius: var(--border-radius);
+          }
+          &:not(:first-child){
+              margin-left: -1px;
+          }
+          &:hover{
+              z-index: 1;
+              border-color:var(--border-color-hover);
+              position: relative;
+          }
+      }
+  }
+
+</style>
