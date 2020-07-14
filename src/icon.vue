275269734f63de6a8cdb50cdd7d7a52f774b8712
wlin00 <616294069@qq.com>
2020-07-14 21:43:47 +0800

    wlin0.0.4版本更新前的提交

--- src/icon.vue
+++ src/icon.vue
@@ -5,6 +5,7 @@
 </template>
 
 <script>
+    import './iconFont'
     export default {
         props: {
             name: {
@@ -17,6 +18,15 @@
 </script>
 
 <style lang="scss" scoped>
+@font-face {
+  font-family: 'wlin-demo';  /* project id 1918365 */
+  src: url('//at.alicdn.com/t/font_1918365_76etf97ersd.eot');
+  src: url('//at.alicdn.com/t/font_1918365_76etf97ersd.eot?#iefix') format('embedded-opentype'),
+  url('//at.alicdn.com/t/font_1918365_76etf97ersd.woff2') format('woff2'),
+  url('//at.alicdn.com/t/font_1918365_76etf97ersd.woff') format('woff'),
+  url('//at.alicdn.com/t/font_1918365_76etf97ersd.ttf') format('truetype'),
+  url('//at.alicdn.com/t/font_1918365_76etf97ersd.svg#wlin-demo') format('svg');
+}
 .icon{
     width: 1em;
     height: 1em;
